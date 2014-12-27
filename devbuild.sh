aa72f3d1cffd21ac58013e2de310c858b61b4192
Adron <adronhall@gmail.com>
2014-12-26 22:47:51 -0800

    Added non-interactive keygen command to create SSH key.

--- devbuild.sh
+++ devbuild.sh
@@ -4,16 +4,23 @@ clear
 
 EMAIL="adronhall@gmail.com"
 USERNAME="Adron Hall"
+INSTALLDIR="~/Apps"
 echo "Using email address: $EMAIL"
-echo "using username as: \"$USERNAME\""
+echo "Using username as: $USERNAME"
+echo "Creating $INSTALLDIR for app installations."
+mkdir $INSTALLDIR
+
+echo "Setting up an SSH key for use."
+ssh-keygen -t rsa -N -f id_rsa -C "$EMAIL"
 
 echo "Insuring git is installed and the latest version."
+add-apt-repository ppa:git-core/ppa
+apt-get update
+apt-get install git
 
-
-echo ""
 echo "Setting git global variables."
-git config --global user.email $EMAIL
-git config --global user.name $USERNAME
+git config --global user.email "$EMAIL"
+git config --global user.name "$USERNAME"
 
 echo "Variables set, configuration complete."
 git config --list
